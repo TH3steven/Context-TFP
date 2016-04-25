1c2dc6b324074403a7e8f302039461e8e183b8fd
TH3steven <stevenmeijer9@gmail.com>
2016-04-25 16:22:03 +0200

    Small restructure of project folders conform standard and added main.
    Additionally, Sprint plan 1 has been converted to a pdf for better
    viewing

---
+++ src/main/java/nl/tudelft/contextproject/ContextTFP.java
@@ -0,0 +1,27 @@
+/**
+ * License information TBD
+ */
+
+package main.java.nl.tudelft.contextproject;
+
+/**
+ * This is the main file for the contextproject of Team Free Pizza.
+ * The main purpose of this project is to allow PolyCast Productions B.V. to
+ * easily control their cameras and improve their digital environment.
+ * 
+ * <p>This file shoud be used to initialize the program.
+ * 
+ * @author Team Free Pizza
+ * @version %I%, %G%
+ */
+public class ContextTFP {
+
+	/**
+	 * The main class of the project. Calling this method will start the program.
+	 * @param args Environment arguments for the main method.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
