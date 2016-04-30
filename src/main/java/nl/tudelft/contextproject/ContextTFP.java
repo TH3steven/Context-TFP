12850ef4861d24d84254ff1ffa8ea0d88958be08
TH3steven <stevenmeijer9@gmail.com>
2016-04-30 18:48:23 +0200

    Created structure for GUI

--- src/main/java/nl/tudelft/contextproject/ContextTFP.java
+++ src/main/java/nl/tudelft/contextproject/ContextTFP.java
@@ -1,27 +1,94 @@
 /**
- * License information TBD
+ * This product is licensed under the CC BY 4.0 license and
+ * may be modified without notice of the original creators. This modified version may
+ * be distributed for commercial use when appropriate credit is given where due.
+ * 
+ * See http://creativecommons.org/licenses/by/4.0/
  */
 
 package main.java.nl.tudelft.contextproject;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * This is the main file for the contextproject of Team Free Pizza.
  * The main purpose of this project is to allow PolyCast Productions B.V. to
- * easily control their cameras and improve their digital environment.
+ * easily control their cameras and to improve their digital environment.
  * 
  * <p>This file shoud be used to initialize the program.
  * 
  * @author Team Free Pizza
  * @version %I%, %G%
+ * @since 0.1
  */
-public class ContextTFP {
+public class ContextTFP extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override()
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("TFP Camera Control");
+
+        initRootLayout();
+        showMenuOverview();
+    }
+
+    /**
+     * Initializes the root layout of the application.
+     */
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ContextTFP.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the menu overview inside the root layout.
+     */
+    public void showMenuOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ContextTFP.class.getResource("view/MenuOverview.fxml"));
+            AnchorPane menuOverview = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(menuOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * The main class of the project. Calling this method will start the program.
+     * 
      * @param args Environment arguments for the main method.
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        launch(args);
+    }
 
+    /**
+     * Get method for the primary stage of the application.
+     * 
+     * @return The main stage
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 }
