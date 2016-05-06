2fbe53f33c632df9a11c85f0b04c5a3ed7427375
TimYuenior <T.Yue@student.tudelft.nl>
2016-05-06 12:51:26 +0200

    Add switching images to preview interface

--- src/main/java/nl/tudelft/contextproject/gui/PreviewController.java
+++ src/main/java/nl/tudelft/contextproject/gui/PreviewController.java
@@ -1,5 +1,9 @@
 package main.java.nl.tudelft.contextproject.gui;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +16,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 import main.java.nl.tudelft.contextproject.ContextTFP;
 
 import java.io.IOException;
@@ -28,14 +33,55 @@ public class PreviewController {
     @FXML ImageView imgTwo;
     @FXML ImageView imgThree;
     @FXML ImageView imgFour;
-
+    @FXML Button play;
+    @FXML Button stop;
+    @FXML Button back;
+    @FXML ChoiceBox<String> cb;
+    @FXML TextField duration;
+    private int currentImg;
+    private Timeline timeline;
+    
     @FXML
     private void initialize() {
         actualCam.setImage(new Image("main/resources/placeholder_picture.jpg"));
         imgOne.setImage(new Image("main/resources/placeholder_picture.jpg"));
-        imgTwo.setImage(new Image("main/resources/placeholder_picture.jpg"));
-        imgThree.setImage(new Image("main/resources/placeholder_picture.jpg"));
-        imgFour.setImage(new Image("main/resources/placeholder_picture.jpg"));
+        imgTwo.setImage(new Image("main/resources/test2.jpg"));
+        imgThree.setImage(new Image("main/resources/test3.jpg"));
+        imgFour.setImage(new Image("main/resources/test4.jpg"));
+        currentImg = 1;
+        addTextLimiter(duration, 2);
+        timeline = new Timeline(new KeyFrame(Duration.millis(2000), ae -> nextImage()));
+        play.setOnAction((event) -> {
+                timeline.play();
+            });
+        stop.setOnAction((event) -> {
+                timeline.stop();
+            });
+        back.setOnAction((event) -> {
+                timeline.stop();
+                MenuController.show();
+            });
+        imgOne.setOnMouseClicked((event) -> {
+                actualCam.setImage(imgOne.getImage());
+                currentImg = 1;
+                timeline.stop();
+            });
+        imgTwo.setOnMouseClicked((event) -> {
+                actualCam.setImage(imgTwo.getImage());
+                currentImg = 2;
+                timeline.stop();
+            });
+        imgThree.setOnMouseClicked((event) -> {
+                actualCam.setImage(imgThree.getImage());
+                currentImg = 3;
+                timeline.stop();
+            });
+        imgFour.setOnMouseClicked((event) -> {
+                actualCam.setImage(imgFour.getImage());
+                currentImg = 4;
+                timeline.stop();
+            });
+        cb.setItems(FXCollections.observableArrayList("Shot 1", "Shot 2", "Shot 3", "Shot 4"));
     }
 
     /**
@@ -51,5 +97,38 @@ public class PreviewController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    /**
+     * shows the next image on the actual camera screen.
+     */
+    public void nextImage() {
+        if (currentImg == 1) {
+            actualCam.setImage(imgTwo.getImage());
+            currentImg++;
+            timeline.getKeyFrames().clear();
+            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5000), ae -> nextImage()));
+            timeline.playFromStart();
+        } else if (currentImg == 2) {
+            actualCam.setImage(imgThree.getImage());
+            currentImg++;
+            timeline.playFromStart();
+        } else if (currentImg == 3) {
+            actualCam.setImage(imgFour.getImage());
+            currentImg++;
+            timeline.playFromStart();
+        }
+    }
+    
+    public static void addTextLimiter(final TextField tf, final int maxLength) {
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (tf.getText().length() > maxLength) {
+                    String s = tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
     }
 }
