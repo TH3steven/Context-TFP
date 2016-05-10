1dc8d36a4a24111d0926af26f870544a3e64bc95
tkolenbrander <T.J.Kolenbrander@student.tudelft.nl>
2016-05-10 17:06:21 +0200

    New presets are now displayed in the table

--- src/main/java/nl/tudelft/contextproject/gui/PresetController.java
+++ src/main/java/nl/tudelft/contextproject/gui/PresetController.java
@@ -1,34 +1,78 @@
 package main.java.nl.tudelft.contextproject.gui;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-
 import main.java.nl.tudelft.contextproject.ContextTFP;
+import main.java.nl.tudelft.contextproject.camera.CameraSettings;
+import main.java.nl.tudelft.contextproject.presets.InstantPreset;
+import main.java.nl.tudelft.contextproject.presets.Preset;
 
 import java.io.IOException;
 
 public class PresetController {
     
-    @FXML private ChoiceBox<Integer> selectCamera;
-    @FXML private ChoiceBox<Integer> presetID;
+    @FXML private ChoiceBox<Integer> cameraSelecter;
+    @FXML private TextField presetID;
+    @FXML private TextField description;
     @FXML private ImageView cameraView;
     @FXML private Button btnBack;
+    @FXML private Button btnSave;
+    @FXML private CheckBox overwrite;
+    @FXML private TableView<Preset> tableView;
+    @FXML private TableColumn<Preset, Integer> presetColumn;
+    @FXML private TableColumn<Preset, String> descColumn;
     
     @FXML
     private void initialize() {
-        selectCamera.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8));
-        presetID.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8));
+        cameraSelecter.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8));
         cameraView.setImage(new Image("main/resources/placeholder_picture.jpg"));
+
+        setFactories();
+        setActions();
+    }
+    
+    private void setFactories() {
+        presetColumn.setCellValueFactory(
+                new PropertyValueFactory<Preset, Integer>("id"));
+        
+        descColumn.setCellValueFactory(
+                new PropertyValueFactory<Preset, String>("description"));
+    }
+    
+    private void setActions() {
+        final ObservableList<Preset> data = FXCollections.observableArrayList();
+        tableView.setItems(data);
+        
         btnBack.setOnAction((event) -> {
             MenuController.show();
         });
+        
+        //TODO: Add newly created preset to camera.
+        btnSave.setOnAction((event) -> {
+            int id = -1;
+            try {
+                id = Integer.parseInt(presetID.getText());
+                presetID.setStyle("");
+                data.add(new InstantPreset(
+                        new CameraSettings(1,1,1,2000),
+                        id,
+                        description.getText()));
+            } catch (Exception e) {
+                presetID.setStyle("-fx-border-color: red;");
+            }
+        });        
     }
 
     /**
