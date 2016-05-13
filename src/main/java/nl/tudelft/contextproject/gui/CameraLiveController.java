25c02f7fd2aab2b78339624c598079e36a66c5f1
TH3steven <stevenmeijer9@gmail.com>
2016-05-13 14:55:57 +0200

    Small fixes in the live camera view, and added back button

--- src/main/java/nl/tudelft/contextproject/gui/CameraLiveController.java
+++ src/main/java/nl/tudelft/contextproject/gui/CameraLiveController.java
@@ -6,38 +6,49 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+
 import main.java.nl.tudelft.contextproject.ContextTFP;
 
 import java.io.IOException;
 
+/**
+ * This class controls the live view of the camera.
+ * 
+ * @since 0.3
+ */
 public class CameraLiveController {
+
+    @FXML private Button btnBack;
     @FXML private Button swap;
+
     @FXML private ImageView bigView;
     @FXML private ImageView smallView;
-    
-    @FXML
-    private void initialize() {
-        System.out.println("test");
+
+    @FXML private void initialize() {
         initializeViews();
         initializeButtons();
     }
-    
+
     private void initializeViews() {
         Image actual = new Image("main/resources/placeholder_picture.jpg");
         bigView.setImage(actual);
-        
+
         Image current = new Image("main/resources/test3.jpg");
         smallView.setImage(current);
     }
-    
+
     private void initializeButtons() {
         swap.setOnAction((event) -> {
             Image three = bigView.getImage();
             bigView.setImage(smallView.getImage());
             smallView.setImage(three);
         });
+        
+        btnBack.setOnAction((event) -> {
+            MenuController.show();
+        });
     }
-    
+
     /**
      * Shows this view.
      */
