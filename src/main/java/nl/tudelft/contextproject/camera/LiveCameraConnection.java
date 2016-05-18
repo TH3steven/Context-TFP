88e2c9ed3b7c18cdfb383bc74ad5203123b06e6a
bvobart <bvanoort@chello.nl>
2016-05-18 17:02:52 +0200

    Added methods allowing HTTP requests to be made and added getCurrentCameraSettings method

--- src/main/java/nl/tudelft/contextproject/camera/LiveCameraConnection.java
+++ src/main/java/nl/tudelft/contextproject/camera/LiveCameraConnection.java
@@ -1,5 +1,11 @@
 package main.java.nl.tudelft.contextproject.camera;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Observable;
 
 /**
@@ -16,9 +22,12 @@ public class LiveCameraConnection extends CameraConnection {
     
     public static final String CAMERA_MODEL = "AW-HE130";
     
+    private static final int READ_TIMEOUT = 5000;
+    
     private String address;
     private boolean connected;
     private boolean autoFocus;
+    private CameraSettings lastKnown;
     
     /**
      * Creates a LiveCameraConnection object. Assumes that the
@@ -33,13 +42,80 @@ public class LiveCameraConnection extends CameraConnection {
     
     @Override
     public boolean setUpConnection() {
-        // TODO Auto-generated method stub
-        return false;
+        try {
+            URL url = buildCamControlURL("QID");
+            if (("OID:" + CAMERA_MODEL).equals(sendRequest(url))) {
+                int hasAutoFocus = Integer.parseInt(sendRequest(buildPanTiltHeadControlURL("#D1")));
+                autoFocus = hasAutoFocus == 1;
+                connected = true;
+                lastKnown = new CameraSettings();
+                return true;
+            }
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
     
     @Override
     public boolean isConnected() {
         return connected;
+    }
+    
+    /**
+     * Builds the URL for the command specified, which must be a command
+     * from section 3.1 Pan-tilt Head Control.
+     * @param command Command to be sent.
+     * @return the formed URL, according to the {@link #address} and the command.
+     * @throws MalformedURLException if the parameter command is null.
+     */
+    private URL buildPanTiltHeadControlURL(String command) throws MalformedURLException {
+        if (command != null) {
+            URL url = new URL("http://" + address + "/cgi-bin/aw_ptz?cmd=" + command + "&res=1");
+            return url;
+        } else {
+            throw new MalformedURLException("Given command is null");
+        }
+    }
+    
+    /**
+     * Builds the URL for the command specified, which must be a command
+     * from section 3.2 Camera Control.
+     * @param command Command to be sent.
+     * @return the formed URL, according to the {@link #address} and the command.
+     * @throws MalformedURLException if the parameter command is null.
+     */
+    private URL buildCamControlURL(String command) throws MalformedURLException {
+        if (command != null) {
+            URL url = new URL("http://" + address + "/cgi-bin/aw_cam?cmd=" + command + "&res=1");
+            return url;
+        } else {
+            throw new MalformedURLException("Given command is null");
+        }
+    }
+    
+    /**
+     * Sends the HTTP request specified in the URL as a POST request.
+     * It waits for a response from the server until a response is
+     * received or until the connection times out, which happens after
+     * the amount of milliseconds specified in {@link #READ_TIMEOUT}.
+     * @param url the URL containing the full HTTP request 
+     * @return the response of the server. 
+     * @throws IOException when something goes wrong in opening the
+     *      the connection or reading the response from the server.
+     * @throws java.net.SocketTimeoutException when a timeout has
+     *      occurred while connecting to the server.
+     */
+    private String sendRequest(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setReadTimeout(READ_TIMEOUT);
+        connection.connect();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = reader.readLine();
+        reader.close();
+        return response;
     }
     
     /**
@@ -55,7 +131,7 @@ public class LiveCameraConnection extends CameraConnection {
         if (!(o instanceof Camera)) {
             return;
         }
-        Camera cam = (Camera) o;
+        //Camera cam = (Camera) o;
         if (arg instanceof CameraSettings) {
             mutateSettings((CameraSettings) arg);
         }
@@ -85,7 +161,22 @@ public class LiveCameraConnection extends CameraConnection {
 
     @Override
     public CameraSettings getCurrentCameraSettings() {
-        // TODO Auto-generated method stub
+        try {
+            URL panTiltUrl = buildPanTiltHeadControlURL("#APC");
+            URL zoomUrl = buildPanTiltHeadControlURL("#GZ");
+            String panTiltRes = sendRequest(panTiltUrl);
+            String zoomRes = sendRequest(zoomUrl);
+            if (panTiltRes != null && panTiltRes.startsWith("aPC") && zoomRes.startsWith("gz")) {
+                int pan = Integer.parseInt(panTiltRes.substring(4, 8), 16);
+                int tilt = Integer.parseInt(panTiltRes.substring(8, 12), 16);
+                int zoom = (int) Math.round((Integer.parseInt(zoomRes.substring(3, 6), 16) - 1365) / 27.3);
+                int focus = lastKnown.getFocus(); //TODO Fix this.
+                return new CameraSettings(pan, tilt, zoom, focus);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
         return null;
     }
     
