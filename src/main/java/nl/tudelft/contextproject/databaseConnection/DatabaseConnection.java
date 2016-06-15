5d973c68800dc7119eaaa8fb8d887251390f9cb0
tkolenbrander <T.J.Kolenbrander@student.tudelft.nl>
2016-06-15 15:20:55 +0200

    Added script uploading

--- src/main/java/nl/tudelft/contextproject/databaseConnection/DatabaseConnection.java
+++ src/main/java/nl/tudelft/contextproject/databaseConnection/DatabaseConnection.java
@@ -1,12 +1,15 @@
 package nl.tudelft.contextproject.databaseConnection;
 
 import nl.tudelft.contextproject.saveLoad.ApplicationSettings;
+import nl.tudelft.contextproject.script.Script;
+import nl.tudelft.contextproject.script.Shot;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Iterator;
 
 public class DatabaseConnection {
     
@@ -73,10 +76,21 @@ public class DatabaseConnection {
     }
     
     /**
+     * If there is no valid connection, reconnect.
+     * @throws SQLException When no connection can be made, this exception will be thrown.
+     */
+    private void checkValid() throws SQLException {
+        if (conn == null || !isValid(100)) {
+            connect();
+        }
+    }
+    
+    /**
      * Updates the counter by adding 1 to it.
      * @throws SQLException When no connection can be made, this exception will be thrown.
      */
     public void updateCounter() throws SQLException {
+        checkValid();
         setCounter(getCounter() + 1);
     }
     
@@ -85,6 +99,7 @@ public class DatabaseConnection {
      * @throws SQLException When no connection can be made, this exception will be thrown.
      */
     public void resetCounter() throws SQLException {
+        checkValid();
         setCounter(0);    
     }
     
@@ -95,9 +110,7 @@ public class DatabaseConnection {
      * @throws SQLException When no connection can be made, this exception will be thrown.
      */
     private void setCounter(int number) throws SQLException {
-        if (conn == null) {
-            throw new SQLException();
-        }
+        checkValid();
         
         Statement stmt = conn.createStatement();
         String query = "UPDATE " + COUNTER_TABLE + " set number='" + number + "'";
@@ -112,9 +125,7 @@ public class DatabaseConnection {
      * @throws SQLException When no connection can be made, this exception will be thrown.
      */
     public int getCounter() throws SQLException {
-        if (conn == null) {
-            throw new SQLException();
-        }
+        checkValid();
         
         Statement stmt = conn.createStatement();
         String query = "SELECT number FROM " + COUNTER_TABLE;
@@ -123,5 +134,42 @@ public class DatabaseConnection {
         rs.close();
         stmt.close();
         return id;
+    }
+    
+    /**
+     * Clears the script in the database and uploads a new script.
+     * 
+     * @param script The script to upload.
+     * @throws SQLException When no connection can be made, this exception will be thrown.
+     */
+    public void uploadScript(Script script) throws SQLException {
+        checkValid();
+        clearScript();
+        
+        Statement stmt = conn.createStatement();
+        Iterator<Shot> iterator = script.getShots().iterator();
+        while (iterator.hasNext()) {
+            Shot shot = iterator.next();
+            StringBuilder sBuilder = new StringBuilder("INSERT INTO " + SCRIPT_TABLE + " VALUES " + "(");
+            sBuilder.append("'" + shot.getNumber() + "',");
+            sBuilder.append("'" + shot.getShotId() + "',");
+            sBuilder.append("'" + shot.getCamera() + "',");
+            sBuilder.append("'" + shot.getPreset() + "',");
+            sBuilder.append("'" + shot.getDescription() + "',");
+            //sBuilder.append("'" + shot.getAction() + "');");
+            stmt.executeUpdate(sBuilder.toString());
+        }
+        stmt.close();
+    }
+    
+    /**
+     * Clears the script table in the database.
+     * @throws SQLException When no connection can be made, this exception will be thrown.
+     */
+    private void clearScript() throws SQLException {
+        Statement stmt = conn.createStatement();
+        String query = "DELETE FROM " + SCRIPT_TABLE;
+        stmt.executeUpdate(query);
+        stmt.close();
     }
 }
