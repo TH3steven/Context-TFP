49f1c3ef2ae9c097c61a41e1cf98ce1d2339f79c
unknown <T.Yue@student.tudelft.nl>
2016-06-15 15:48:30 +0200

    Camera live feed UI now shows stream instead of images

--- src/main/java/nl/tudelft/contextproject/gui/CameraFeedsController.java
+++ src/main/java/nl/tudelft/contextproject/gui/CameraFeedsController.java
@@ -1,17 +1,18 @@
 package nl.tudelft.contextproject.gui;
 
+import javafx.application.Platform;
+import javafx.beans.property.FloatProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import nl.tudelft.contextproject.ContextTFP;
 import nl.tudelft.contextproject.camera.Camera;
-import nl.tudelft.contextproject.script.Script;
-import nl.tudelft.contextproject.script.Shot;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -23,10 +24,16 @@ public class CameraFeedsController {
     @FXML private ChoiceBox<Camera> camChoiceOne;
     @FXML private ChoiceBox<Camera> camChoiceTwo;
     
-    @FXML private ImageView tempOne;
-    @FXML private ImageView tempTwo;
+    @FXML private ImageView viewOne;
+    @FXML private ImageView viewTwo;
+    
+    @FXML private VBox streamBoxOne;
+    @FXML private VBox streamBoxTwo;
     
     private Collection<Camera> cameras;
+    
+    private LiveStreamHandler leftStreamHandler;
+    private LiveStreamHandler rightStreamHandler;
     
     /**
      * Initialize method used by JavaFX.
@@ -34,17 +41,11 @@ public class CameraFeedsController {
     @FXML private void initialize() {
         cameras = Camera.getAllCameras();
         
-        initImages();
+        leftStreamHandler = new LiveStreamHandler();
+        rightStreamHandler = new LiveStreamHandler();
+        
         initChoiceBoxes();
-    }
-    
-    /**
-     * Initializes temporary imageviews.
-     * These will get replaces by camera live feeds.
-     */
-    private void initImages() {
-        tempOne.setImage(loadImage("black.png"));
-        tempTwo.setImage(loadImage("black.png"));
+        initStreams();
     }
     
     /**
@@ -65,31 +66,117 @@ public class CameraFeedsController {
         camChoiceOne.setValue(Camera.DUMMY);
         camChoiceTwo.setValue(Camera.DUMMY);
         
-        addListener(camChoiceOne);
-        addListener(camChoiceTwo);
+        addChoiceListener(camChoiceOne);
+        addChoiceListener(camChoiceTwo);
+    }
+    
+    private void initStreams() {
+        
+        addStreamListeners();
     }
     
     /**
      * Add a changelistener to the choicebox.
      * @param cb the choicebox to add the listener to.
      */
-    private void addListener(ChoiceBox<Camera> cb) {
+    private void addChoiceListener(ChoiceBox<Camera> cb) {
         cb.getSelectionModel().selectedItemProperty().addListener((obs, oldV, newV) -> {
-            System.out.println("Temporary stream print: " + newV.getConnection().toString());
+            String newStream = newV.getConnection().getStreamLink();
+            VBox outerBox = (VBox) cb.getParent();
+            VBox innerBox = (VBox) outerBox.getChildren().get(1);
+            ImageView oldStream = (ImageView) innerBox.getChildren().get(0);
+            
+            LiveStreamHandler streamHandler;
+            
+            if (cb.equals(camChoiceOne)) {
+                streamHandler = leftStreamHandler;
+            } else {
+                streamHandler = rightStreamHandler;
+            }
+            
+            updateStream(newStream, oldStream, streamHandler);
+        });
+    }
+    
+    private void addStreamListeners() {
+        ImageView leftView = (ImageView) streamBoxOne.getChildren().get(0);
+        ImageView rightView = (ImageView) streamBoxTwo.getChildren().get(0);
+        
+        streamBoxOne.widthProperty().addListener((observable, oldValue, newValue) -> {
+            if (leftStreamHandler != null) {
+                fitImageViewSize(newValue.floatValue(), 
+                        (float) streamBoxOne.getHeight(), leftView, leftStreamHandler);
+            }
+        });
+
+        streamBoxTwo.widthProperty().addListener((observable, oldValue, newValue) -> {
+            if (rightStreamHandler != null) {
+                fitImageViewSize(newValue.floatValue(), 
+                        (float) streamBoxTwo.getHeight(), rightView, rightStreamHandler);
+            }
+        });
+        
+        streamBoxOne.heightProperty().addListener((observable, oldValue, newValue) -> {
+            if (leftStreamHandler != null) {
+                fitImageViewSize((float) streamBoxOne.getWidth(), 
+                        newValue.floatValue(), leftView, leftStreamHandler);
+            }
+        });
+        
+        streamBoxTwo.heightProperty().addListener((observable, oldValue, newValue) -> {
+            if (rightStreamHandler != null) {
+                fitImageViewSize((float) streamBoxTwo.getWidth(), 
+                        newValue.floatValue(), rightView, rightStreamHandler);
+            }
         });
     }
     
     /**
-     * Loads an image. Loads an error image if path is null or invalid.
-     * 
-     * @param path Image path.
-     * @return The newly loaded image.
+     * Updates the camera stream to the stream referenced by the specified link.
+     * @param streamLink the link to the video stream to be played next.
      */
-    private Image loadImage(String path) {
-        try {
-            return new Image(path);
-        } catch (IllegalArgumentException e) {
-            return new Image("error.jpg");
+    private void updateStream(String streamLink, ImageView imageView, LiveStreamHandler streamHandler) {
+        VBox vBox = (VBox) imageView.getParent();
+        
+        if (streamHandler != null) {
+            streamHandler.stop();
+        }
+
+        ImageView newView = streamHandler.createImageView(streamLink, 1920, 1080);
+        
+        Platform.runLater(() -> {
+            fitImageViewSize((float) vBox.getWidth(), (float) vBox.getHeight(), newView, streamHandler);
+        }); 
+        
+        vBox.getChildren().clear();
+        vBox.getChildren().add(newView);
+        
+        streamHandler.start();
+    }
+    
+    /**
+     * Resizes the ImageView.
+     * @param width The new width of the ImageView.
+     * @param height The new height of the ImageView.
+     * @param imageView The imageView that needs resizing.
+     * @param streamHandler The LiveStreamHandler responsible for the stream.
+     */
+    private void fitImageViewSize(float width, float height, ImageView imageView, LiveStreamHandler streamHandler) {
+        if (imageView.getImage() instanceof WritableImage && streamHandler.isPlaying()) {
+            FloatProperty videoSourceRatioProperty = streamHandler.getRatio();
+            float fitHeight = videoSourceRatioProperty.get() * width;
+            if (fitHeight > height) {
+                imageView.setFitHeight(height);
+                double fitWidth = height / videoSourceRatioProperty.get();
+                imageView.setFitWidth(fitWidth);
+                imageView.setX((width - fitWidth) / 2);
+                imageView.setY(0);
+            } else {
+                imageView.setFitWidth(width);
+                imageView.setFitHeight(fitHeight);
+                imageView.setY((height - fitHeight) / 2);
+                imageView.setX(0);
+            } 
         }
     }
     
