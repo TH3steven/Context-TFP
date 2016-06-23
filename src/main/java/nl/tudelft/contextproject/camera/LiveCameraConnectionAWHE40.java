73dcf4519e6189c91eb3a5d07f8ad4274ea30049
bvobart <bvanoort@chello.nl>
2016-06-23 18:09:21 +0200

    Forgot to save this one

--- src/main/java/nl/tudelft/contextproject/camera/LiveCameraConnectionAWHE40.java
+++ src/main/java/nl/tudelft/contextproject/camera/LiveCameraConnectionAWHE40.java
@@ -1,11 +1,33 @@
 package nl.tudelft.contextproject.camera;
 
+import java.io.IOException;
+
 public class LiveCameraConnectionAWHE40 extends LiveCameraConnection {
 
     public static final String CAMERA_MODEL = "AW-HE40";
     
     public LiveCameraConnectionAWHE40(String address) {
         super(address);
+    }
+    
+    @Override
+    public boolean setUpConnection() {
+        try {
+            String cameraModel = sendRequest(buildCamControlURL("QID"));
+
+            if (cameraModel.equals("OID:" + CAMERA_MODEL)) {
+                setConnected(true);
+                setLastKnownSettings(new CameraSettings());
+                setLastKnownSettings(getCurrentCameraSettings());
+                hasAutoFocus();
+                return true;
+            }
+
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
     
     @Override
